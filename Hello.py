b2b4cdb37a2380493e1933931d056c2ab28bc87d
davidzhu22 <90352709+davidzhu22@users.noreply.github.com>
2022-05-01 19:21:39 -0700

    add two line code

--- Hello.py
+++ Hello.py
@@ -3,3 +3,8 @@ print("You are a genius person")
 print("You can stand and sleep")
 
 print("Third Party change the code")
+
+for i in range(10):
+    print("I purchase a sound car")
+
+print("I need to install machine, I do not know it")
